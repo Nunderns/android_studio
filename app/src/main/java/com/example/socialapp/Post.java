5631d1a0f6c766baf87bbed5b562431427a565be
Henri Okayama <henri.okayama@gmail.com>
2025-04-27 18:46:51 -0300

    feat: Criação da função de detalhes dos posts

--- app/src/main/java/com/example/socialapp/Post.java
+++ app/src/main/java/com/example/socialapp/Post.java
@@ -4,10 +4,33 @@ public class Post {
     public String autor;
     public String conteudo;
     public int imagemResId;
+    private int comentarios;
+    private int curtidas;
+    private int favoritos;
 
-    public Post(String autor, String conteudo, int imagemResId) {
+    public Post(String autor, String conteudo, int imagemResId, int comentarios, int curtidas, int favoritos) {
         this.autor = autor;
         this.conteudo = conteudo;
         this.imagemResId = imagemResId;
+        this.comentarios = comentarios;
+        this.curtidas = curtidas;
+        this.favoritos = favoritos;
+    }
+
+    // Getters
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public int getComentarios() {
+        return comentarios;
+    }
+
+    public int getCurtidas() {
+        return curtidas;
+    }
+
+    public int getFavoritos() {
+        return favoritos;
     }
 }
